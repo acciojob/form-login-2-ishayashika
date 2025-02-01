cc01402cdeb45290ec33c8f1bef37a99bbaf5d39
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-02-01 17:54:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let fname=document.getElementById("fname");
+let lname=document.getElementById("lname");
+let phone=document.getElementById("phone");
+let mail=document.getElementById("mail");
+function submit(){
+	let a=fname.value;
+	let b=lname.value;
+	let c=phone.value;
+	let d=mail.value;
+	alert("First Name: "+a+ " Last Name: "+b+ " Phone Number: "+c+ " Email ID: "+d);
+	
+}
